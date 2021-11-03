6f9956992fda48c56054443473828e4c75ac8f15
XOiRD <a.f.bahemdan@gmail.com>
2021-11-03 13:35:19 +0400

    updata right navbar

--- src/components/partials/dashboard/sidebarStyle/rightsidebar.js
+++ src/components/partials/dashboard/sidebarStyle/rightsidebar.js
@@ -27,7 +27,7 @@ const RightSidebar = () => {
                                     </div>
                                     <div className="ms-3">
                                         <h6 className="mb-0">Anna Sthesia</h6>
-                                        <p className="mb-0">Just Now</p>
+                                        <p className="mb-0">before three minutes</p>
                                     </div>
                                 </div>
                                 <div className="d-flex align-items-center mb-4">
