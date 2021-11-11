4a79c7788b57cc0b3145f18b377c0f9f2d387681
XOiRD <a.f.bahemdan@gmail.com>
2021-11-11 12:31:53 +0400

    settings update

--- README.md
+++ README.md
@@ -68,3 +68,13 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+{
+        "AboutAce": [
+            { "title": "Aboutfollowme", "subtitle": ["Company Introduction"] },
+            { "title": "Legal Notifications", "subtitle": [{"heading":"Terms & Conditions"}, {"heading":"Privacy"}, {"heading":"Disclaimer"}, {"heading":"Rules & Regulation"}] },
+            { "title": "Page", "subtitle": [{"heading":"Brand Page Policy"}, {"heading":"Brand Page Policy"}] },
+            { "title": "others", "subtitle": ["Career", "Contact Us"] }
+        ]
+    },
--- src/views/dashboard/app/settings.js
+++ src/views/dashboard/app/settings.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const Settings = () => {
    const [content,setContent] = useState([]);
    let [dynamic,setDynamic] = useState([]);
+   const [show,setShow]=useState(false)
 
     useEffect(()=>{
         fetch('/data/data.json')
@@ -12,16 +13,15 @@ const Settings = () => {
     },[])
    
     const Filterstate = (index,str)=>{
-    
         setDynamic(content[index][str])
     }
+{console.log('this is dynamic')}
 {console.log(dynamic)}
+{console.log('this is all data')}
+{console.log(content)}
   return (
 
     <div style={{width:"75%",margin:'0 auto',height:'100%',display:'flex'}}>
-
-        
-     
 
        <div style={{marginLeft:'50px ',width:"25%", height:'100vh',boxShadow:'0 0 1px 0 rgb(10,20,30)'}} >
 
@@ -62,13 +62,20 @@ const Settings = () => {
 {dynamic.map(i=>(
    
     <ul key={Math.random(10)} className="list-group" style={{borderBottom:"4px solid #ccc"}}>
-        
-    <li className="list-group-item"  style={{fontWeight:"500",fontSize:"16px",color:"#50b5ff" }}><span>{i.title}</span></li>
+    
+    <li  className="list-group-item"  style={{fontWeight:"500",fontSize:"16px",color:"#50b5ff" }}><span>{i.title}</span></li>
     
    { i.subtitle.length >0 && i.subtitle.map((val) =>(
-    <ul className="list-group-item" >
-   
-   <li style={{listStyleType:"none"}}> { val} </li>
+    <ul className="list-group-item" style={{display:'flex',justifyContent:'space-between',flexDirection:'column'}} >
+   <li style={{listStyle:'none'}}>{val.heading}</li>
+   <button style={{border:'none',background:'none',outline:'none'}} onClick={()=>setShow(!show)}>{show ? <i className="fas fa-chevron-up"></i>:<i className="fas fa-chevron-down"></i>}</button>
+
+
+
+{show ? 
+<li>{val.para}</li>
+:null}
+
   </ul>
    ))
     
@@ -78,40 +85,7 @@ const Settings = () => {
 ))} 
 </div>
 
-{/* <div style={{width:"50%", height:'100vh',boxShadow:'0 0 1px 0 rgb(10,20,30)'}} >
 
-<div >  
-<ul className="list-group  " >
-<li className="list-group-item"> <span>Account</span></li>
-<li className="list-group-item"> <span>Login and Secuirty</span></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"> <Link to ="#" className="d-block w-100">Edit Profile</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"><Link to ="#" className="d-block w-100">Nick Name</Link><i className="fas fa-chevron-right text-muted "></i></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"><Link to ="#" className="d-block w-100">verfication</Link><i className="fas fa-chevron-right  text-muted"></i></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"><Link to ="#" className="d-block w-100">Phone Number</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"><Link to ="#" className="d-block w-100">Email</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"><Link to ="#" className="d-block w-100">Password</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-</ul>
-</div>
-
-<div >  
-<ul className="list-group   " >
-<li className="list-group-item"> <span>Social Account</span></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"> <Link to ="#" className="d-block w-100">Google</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"><Link to ="#" className="d-block w-100">FaceBook</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-
-</ul>
-</div>
-<div >  
-<ul className="list-group   " >
-<li className="list-group-item"> <span>Data</span></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"> <Link to ="#" className="d-block w-100">Language</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-<li className="list-group-item d-flex justify-content-between align-items-center"><Link to ="#" className="d-block w-100">Country and Region</Link> <i className="fas fa-chevron-right text-muted "></i></li>
-
-</ul>
-</div>
-
-</div> */}
-{/* /////////////////////// */}
     </div>
   );
 };
