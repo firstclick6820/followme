1a8a08225f6025ee9cecc82a04b735234e3ccacd
abhiramisusith <52876695+abhiramisusith@users.noreply.github.com>
2021-11-15 12:12:10 +0400

    get updated

--- src/api/followfollower/followfollower.js
+++ src/api/followfollower/followfollower.js
@@ -4,8 +4,8 @@ const apis = axios.create({
     baseURL: 'http://192.168.0.115/api/FollowFollower'
 })
 
-const getFollow = () => apis.get("/Follow")
-const getUnFollow = () => apis.get("/UnFollow")
+const getFollow = payload => apis.get(`/Follow?FollowUser_Id=${payload}`)
+const getUnFollow = payload => apis.get(`/UnFollow?UnFollowUser_Id=${payload}`)
 const getGetFollowers = () => apis.get("/GetFollowers")
 const getGetFollowings = () => apis.get("/GetFollowings")
 const getGetTopFollowers = () => apis.get("/GetTopFollowers")
--- src/api/notifications/notifications.js
+++ src/api/notifications/notifications.js
@@ -4,9 +4,9 @@ const apis = axios.create({
     baseURL: 'http://192.168.0.115/api/Notifications'
 })
 
-const getGetNotifications = () => apis.get("/GetNotifications")
-const getMarkNotificationAsRead = () => apis.get("/MarkNotificationAsRead")
-const getUserNoticationsOnOff = () => apis.get("/UserNoticationsOnOff")
+const getGetNotifications = payload => apis.get(`/GetNotifications?UserId=${payload}&SignInType=${payload}`)
+const getMarkNotificationAsRead = payload => apis.get(`/MarkNotificationAsRead?NotificationId=${payload}`)
+const getUserNoticationsOnOff = payload => apis.get(`/UserNoticationsOnOff?UserId=${payload}&SignInType=${payload}&On=${payload}`)
 
 
 
